623563dd2b083505bc2d021bf6fed1808f33d32c
tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2023-08-05 06:02:10 +0200

    concept 1 and bunch of other stuff

---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Title({children}) {
+    return (
+        <center><strong>{children}</strong></center>
+    );
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -3,6 +3,7 @@ import MDXComponents from '@theme-original/MDXComponents';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
+import Title from '/src/components/Title.js';
 import Raidwide from '/src/components/Raidwide.js';
 import Tankbuster from '/src/components/Tankbuster.js';
 import Role from '/src/components/Role.js';
@@ -13,6 +14,7 @@ export default {
   Raidwide,
   Tankbuster,
   Role,
+  Title,
   Tabs,
   TabItem
 };
