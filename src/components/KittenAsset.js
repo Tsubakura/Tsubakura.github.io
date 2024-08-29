4f70770be60dea4c5eb4d8be53f266d04ac29904
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2024-08-29 23:39:13 +0200

    Updated assets to use webm now

--- src/components/KittenAsset.js
+++ src/components/KittenAsset.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 
-export default function image({children, path}) {
+export default function image({children, path, wrapper = 'span'}) {
+    let assetURL = 'https://ffxivkittenassets.z7.web.core.windows.net/'
     return (
-        <img src={`https://ffxivkittenassets.z7.web.core.windows.net/${path}`} />
+        path.endsWith("webm") ? <ReactPlayer url={`${assetURL}${path}`} wrapper={wrapper} playsinline={true} muted={true} loop={true} controls={true} playing={true} /> : <img src={`${assetURL}${path}`} />
     );
 }
