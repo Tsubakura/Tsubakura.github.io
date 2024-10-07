5ab4966ae45a434e5510c2fb60727a6a58115130
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2024-10-07 03:41:08 +0200

    Added a way to add width in assets

--- src/components/KittenAsset.js
+++ src/components/KittenAsset.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactPlayer from 'react-player'
 
-export default function image({children, path, wrapper = 'span'}) {
+export default function image({children, path, wrapper = 'span', width}) {
     let assetURL = 'https://ffxivkittenassets.z7.web.core.windows.net/'
     return (
-        path.endsWith("webm") ? <ReactPlayer url={`${assetURL}${path}`} wrapper={wrapper} playsinline={true} muted={true} loop={true} controls={true} playing={true} /> : <img src={`${assetURL}${path}`} />
+        path.endsWith("webm") ? <ReactPlayer url={`${assetURL}${path}`} wrapper={wrapper} playsinline={true} muted={true} loop={true} controls={true} playing={true} /> : (width) ? <img src={`${assetURL}${path}`} width={width} /> : <img src={`${assetURL}${path}`} />
     );
 }
