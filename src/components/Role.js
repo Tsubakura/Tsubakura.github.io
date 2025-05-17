f38b0e244139ff4a13f3039add46a390ae34293b
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2025-05-17 20:41:37 +0200

    Lone wolf lament

--- src/components/Role.js
+++ src/components/Role.js
@@ -8,6 +8,10 @@ import D1 from '/img/assets/00_ROLE/D1.png';
 import D2 from '/img/assets/00_ROLE/D2.png';
 import D3 from '/img/assets/00_ROLE/D3.png';
 import D4 from '/img/assets/00_ROLE/D4.png';
+import Tank from '/img/assets/00_ROLE/Tank.png';
+import Healer from '/img/assets/00_ROLE/Healer.png';
+import Support from '/img/assets/00_ROLE/Support.png';
+import DPS from '/img/assets/00_ROLE/DPS.png';
 
 export default function Role({children, role}) {
     let returnValue = '';
@@ -21,6 +25,10 @@ export default function Role({children, role}) {
         case 'D2': returnValue = D2; break;
         case 'D3': returnValue = D3; break;
         case 'D4': returnValue = D4; break;
+        case 'Tank': returnValue = Tank; break;
+        case 'Healer': returnValue = Healer; break;
+        case 'Support': returnValue = Support; break;
+        case 'DPS': returnValue = DPS; break;
     }
     return (
         <img src={returnValue} />
