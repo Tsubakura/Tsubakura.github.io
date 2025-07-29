cc235b16491eb4f7a5aa91a3d2c00771943fb595
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2025-07-29 21:15:07 +0200

    Update KittenAsset.js

--- src/components/KittenAsset.js
+++ src/components/KittenAsset.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactPlayer from 'react-player'
 
 export default function image({children, path, wrapper = 'span', width}) {
-    let assetURL = 'https://ffxivkittenassets.z7.web.core.windows.net/'
+    //'https://ffxivkittenassets.z7.web.core.windows.net/'
+    //https://ffxivkittenassetcdn.azureedge.net/
+    //https://ffxivkittenassetcdn-h6e7b4h5bbfxakgm.z01.azurefd.net/
+    let assetURL = 'https://ffxivkittenassetcdn-h6e7b4h5bbfxakgm.z01.azurefd.net/'
     return (
         path.endsWith("webm") ? <ReactPlayer url={`${assetURL}${path}`} wrapper={wrapper} playsinline={true} muted={true} loop={true} controls={true} playing={true} /> : (width) ? <img src={`${assetURL}${path}`} width={width} /> : <img src={`${assetURL}${path}`} />
     );
