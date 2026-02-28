35a8d51591aaa3571d38cd937c31ceb121c420d1
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2026-02-28 05:13:18 +0100

    Updated Role component

--- src/components/Role.js
+++ src/components/Role.js
@@ -8,8 +8,11 @@ import D1 from '/img/assets/00_ROLE/D1.png';
 import D2 from '/img/assets/00_ROLE/D2.png';
 import D3 from '/img/assets/00_ROLE/D3.png';
 import D4 from '/img/assets/00_ROLE/D4.png';
+import Any from '/img/assets/00_ROLE/AnyRole.png';
 import Tank from '/img/assets/00_ROLE/Tank.png';
+import Melee from '/img/assets/00_ROLE/MeleeRole.png';
 import Healer from '/img/assets/00_ROLE/Healer.png';
+import Ranged from '/img/assets/00_ROLE/RangedRole.png';
 import Support from '/img/assets/00_ROLE/Support.png';
 import DPS from '/img/assets/00_ROLE/DPS.png';
 
@@ -25,12 +28,15 @@ export default function Role({children, role}) {
         case 'D2': returnValue = D2; break;
         case 'D3': returnValue = D3; break;
         case 'D4': returnValue = D4; break;
+        case 'Any': returnValue = Any; break;
         case 'Tank': returnValue = Tank; break;
+        case 'Melee': returnValue = Melee; break;
         case 'Healer': returnValue = Healer; break;
+        case 'Ranged': returnValue = Ranged; break;
         case 'Support': returnValue = Support; break;
         case 'DPS': returnValue = DPS; break;
     }
     return (
-        <img src={returnValue} />
+        <img src={returnValue} width='36px' />
     );
 }
